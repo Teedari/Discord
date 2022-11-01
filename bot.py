e5aa5589d21408087ed67d886248e06d34f28830
GoDari <godfred@xpathire.com>
2022-11-01 19:36:01 +0000

    events lesson

--- bot.py
+++ bot.py
@@ -6,4 +6,18 @@ bot = hikari.GatewayBot(token=config('DISCORD_BOT_TOKEN'))
 
 # Events goes here
 
+@bot.listen()
+async def bot_initiate(event: hikari.StartedEvent):
+  print("BOT INITIATED.....")
+
+@bot.listen()
+async def greeting(event: hikari.GuildMessageCreateEvent) -> None:
+  if event.is_bot or not event.content:
+    return
+  
+  if event.content.startswith('@bot'):
+    await event.message.respond("Hey i'm a bot")
+  print("BOT INITIATED")
+
+
 bot.run()
